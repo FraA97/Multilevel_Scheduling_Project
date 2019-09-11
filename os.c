18c69973da6b09a8d822748fec854ad8bc761e04
Francesco <artibani.1743611@studenti.uniroma1.it>
2019-09-11 10:12:47 +0200

    added OS_step and check_end functions

--- os.c
+++ os.c
@@ -23,3 +23,82 @@ OS* OS_init(int argc, char ** argv,FILE* f){
 	os->running_event = NULL;
 
 }
+
+
+int OS_step(OS* os,FILE * f){
+	fprintf(f,"************************************************************************************\n");
+	fprintf(f,"STEP: %d \n",os->step);
+	OS_print_info( os,f); //TODO
+
+	os->step ++;
+	//check end
+	if(check_end(os) == 1){ 
+		fprintf(f,"END");
+		return 0;
+	}
+
+	//check for new process
+	new_arrivals(os); //TODO
+	
+
+	//check running process
+	if(os->running == NULL ){
+		printf("getting next running\n");
+		if(get_next_running(os) == 0) //TODO
+			printf("no process running\n");
+		else {
+			os->running->in_status = 0;
+			printf("got next running: %d\n",os->running->pid);
+		}
+	}
+
+	else{
+		os->running->in_status ++;
+
+		printf("running %d", os->running->pid);	
+		printf("for %d\n", os->running->in_status);
+
+		Event* e=(Event*) os->running->events->head;
+		printf("running has a burst %d long\n",e->duration);
+
+	 	if(os->running->in_status >= os->q){
+	 		if(quantum_end(os)==0) //TODO
+				printf("no process running\n");
+			else printf("got next running\n");
+		}
+
+	 	else if( os->running->in_status >= e->duration){
+			if(burst_end(os)== 0) //TODO
+				printf("no process running\n");
+			else {
+				os->running->in_status = 0;
+				printf("got next running\nos->running->in status = %d\n",os->running->in_status );
+			}	
+		}
+	 	
+	 	else
+	 		printf("not changing running\n");
+		
+	}
+	//manage the waiting lists
+	waiting(os); //TODO
+
+	// manage the IO
+	io(os); //TODO
+
+	return 1;
+}
+
+
+int check_end(OS* os){
+	if(os->step <= 1)
+		return 0;
+	for(int i= 0; i<4; i++){
+		if(os->waiting[i]->lenght >0)
+			return 0;
+	}
+	if(os->running != NULL) return 0;
+	if(os->IO->lenght > 0) return 0;
+	if(os->toArrive->lenght > 0) return 0;
+	return 1;
+}
