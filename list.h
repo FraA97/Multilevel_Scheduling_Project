cb24f75082f03e0fc39064161367ff22e70e9dbf
marcocerino <cerino.marco97@gmail.com>
2019-09-12 15:53:33 +0200

    added List_find

--- list.h
+++ list.h
@@ -14,8 +14,10 @@ typedef struct list_head{
 }ListHead;
 
 ListHead* List_init();//initialize the list
-ListElem* pop(ListHead* list);//take the first elem in the list
+ListElem* List_find(ListHead* head, ListElem* item);
 ListElem* detach(ListHead* list, ListElem* elem);
+ListElem* pop(ListHead* list);//take the first elem in the list
+
 int pushFront(ListHead* list, ListElem* elem);//insert a new elem at the head of the list
 int pushBack(ListHead* list, ListElem* elem); //insert a new elem at the end of the list
 int isEmpty(ListHead* list);
