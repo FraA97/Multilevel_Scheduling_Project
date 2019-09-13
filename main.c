58c82d3d7c99ecf9c1bbc96245fe10661e5b0de2
marcocerino <cerino.marco97@gmail.com>
2019-09-13 15:15:09 +0200

    added main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "os.h"
+
+void main(int argc, char ** argv){
+	if(argc < 2){
+		printf("ERRORE: passare almeno un nome di un file");
+		return;
+	}
+
+	FILE * f = fopen("results.txt","w");
+	OS* os = OS_init(argc,argv,f);	
+	printf("os initialized\n");
+	while(OS_step(os,f)!= 0){
+		printf("next step\n");
+	}
+	fclose(f);
+	printf("completato, i risultati si trovano nel file results.txt\n");
+}
